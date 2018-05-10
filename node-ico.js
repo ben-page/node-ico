40f83e085dcc5b8c88580304ec85ed76cbeca01e
Ben Page <bpage@dewalch.com>
2018-05-10 14:10:10 -0500

    updated for recent node

--- node-ico.js
+++ node-ico.js
@@ -1,25 +1,48 @@
-var Promise = require('bluebird');
-var Buffer = require('buffer').Buffer;
-var fs = require('fs');
+/* eslint-disable no-process-exit */
+const Buffer = require('buffer').Buffer;
+const fs = require('fs');
 
 if (process.argv.length < 4) {
     console.error('arguments: output.ico input1.png input2.png ...');
     process.exit();
 }
 
-var output = process.argv[2];
+const output = process.argv[2];
 
-var inputs = process.argv.slice(3);
+const inputs = process.argv.slice(3);
 
-var readFile = Promise.promisify(fs.readFile, fs);
-var writeFile = Promise.promisify(fs.writeFile, fs);
+const readFile = (path, options) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, options, (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
 
-var parse = function (fileName) {
+            resolve(data);
+        });
+    });
+};
+
+const writeFile = (file, path, options) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, path, options, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve();
+        });
+    });
+};
+
+const parse = function (fileName) {
     return readFile(fileName, undefined)
-        .then(function (buf) {
-            var offset = 8; //skip header
+        .then(buf => {
+            let offset = 8; //skip header
 
-            var data = {
+            const data = {
                 name: fileName,
                 buffer: buf,
                 width: undefined,
@@ -28,16 +51,16 @@ var parse = function (fileName) {
             };
 
             while (data.width === undefined || data.paletteCount === undefined) {
-                var length = buf.readUInt32BE(offset);
+                const length = buf.readUInt32BE(offset);
                 offset += 4;
-                var type = buf.toString('ASCII', offset, offset + 4);
+                const type = buf.toString('ASCII', offset, offset + 4);
                 offset += 4;
 
                 switch (type) {
                     case 'IHDR':
                         data.width = buf.readUInt32BE(offset);
                         data.height = buf.readUInt32BE(offset + 4);
-                        var colorType = buf.readInt8(offset + 9);
+                        const colorType = buf.readInt8(offset + 9);
 
                         if (colorType !== 3)
                             data.paletteCount = 0;
@@ -54,68 +77,59 @@ var parse = function (fileName) {
         });
 };
 
-var files = [];
 
-for (var i = 0; i < inputs.length; i++) {
-    files.push(parse(inputs[i]));
-}
+(async function () {
+    let total = 0;
+    const files = [];
 
-Promise.all(files)
-    .then(function (files) {
-        var i, file, total = 0;
-        for (i = 0; i < files.length; i++) {
-            file = files[i];
+    for (let i = 0; i < inputs.length; i++) {
+        const file = await parse(inputs[i]);
+        files.push(file);
+        total += file.buffer.length;
+    }
 
-            total += file.buffer.length;
-        }
+    const headerBytes = 6 + (16 * files.length);
+    const buf = Buffer.alloc(total + headerBytes);
+    let index = 0;
 
-        var headerBytes = 6 + (16 * files.length);
-        var buf = new Buffer(total + headerBytes);
-        var index = 0;
+    const writeUInt8 = value => {
+        buf.writeUInt8(value, index);
+        index++;
+    };
 
-        function writeUInt8(value) {
-            buf.writeUInt8(value, index);
-            index++;
-        }
+    const writeUInt16LE = value => {
+        buf.writeUInt16LE(value, index);
+        index += 2;
+    };
 
-        function writeUInt16LE(value) {
-            buf.writeUInt16LE(value, index);
-            index += 2;
-        }
+    const writeUInt32LE = value => {
+        buf.writeUInt32LE(value, index);
+        index += 4;
+    };
 
-        function writeUInt32LE( value) {
-            buf.writeUInt32LE(value, index);
-            index += 4;
-        }
+    //ICONDIR
+    writeUInt16LE(0); //Reserved
+    writeUInt16LE(1); //type = ICO
+    writeUInt16LE(files.length); //number of images
 
-        //ICONDIR
-        writeUInt16LE(0); //Reserved
-        writeUInt16LE(1); //type = ICO
-        writeUInt16LE(files.length); //number of images
+    let fileOffset = headerBytes;
+    //ICONDIRENTRY
+    for (const file of files) {
+        writeUInt8(file.width === 256 ? 0 : file.width);
+        writeUInt8(file.height === 256 ? 0 : file.height);
+        writeUInt8(file.paletteCount);
+        writeUInt8(0); //reserved
+        writeUInt16LE(0); //color planes
+        writeUInt16LE(0); //bits per pixel - let the viewer decide from PNG file
+        writeUInt32LE(file.buffer.length); //image size
+        writeUInt32LE(fileOffset);
 
-        var fileOffset = headerBytes;
-        //ICONDIRENTRY
-        for (i = 0; i < files.length; i++) {
-            file = files[i];
-            writeUInt8(file.width === 256 ? 0 : file.width);
-            writeUInt8(file.height === 256 ? 0 : file.height);
-            writeUInt8(file.paletteCount);
-            writeUInt8(0); //reserved
-            writeUInt16LE(0); //color planes
-            writeUInt16LE(0); //bits per pixel - let the viewer decide from PNG file
-            writeUInt32LE(file.buffer.length); //image size
-            writeUInt32LE(fileOffset);
+        file.buffer.copy(buf, fileOffset);
 
-            file.buffer.copy(buf, fileOffset);
+        fileOffset += file.buffer.length;
+    }
 
-            fileOffset += file.buffer.length;
-        }
+    await writeFile(output, buf);
 
-        return writeFile(output, buf);
-    })
-    .then(function () {
-        console.log('done');
-    });
-
-
-
+    console.log('done');
+})();
